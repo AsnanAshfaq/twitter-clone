711613027582ec8a06967054dbcdae062b99e306
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2020-09-24 02:27:05 +0500

    Image Uploading functionality :bulb:

--- src/FeedHeader.js
+++ src/FeedHeader.js
@@ -1,11 +1,70 @@
-import React, { useState } from "react";
-import { Avatar } from "@material-ui/core";
+import React, { useEffect, useRef, useState } from "react";
+import { Avatar, Badge } from "@material-ui/core";
 import "./Feed.css";
-import db from "./firebase";
+import { db, storage } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function FeedHeader() {
   const [PostText, setPostText] = useState("");
   const [PostImage, setPostImage] = useState("");
+  const [UserImage, setUserImage] = useState("");
+
+  // reference for image uploading input badge 🔺
+  const imageRef = useRef(null);
+
+  // global state 💯
+  const [{ user }] = useStateValue();
+
+  const findUser = async () => {
+    const snapShot = await db.collection("users").get();
+
+    const userDocument = await snapShot.docs.map((doc) => {
+      const currentDocument = doc.data();
+      // get the data of the respective user from the cloud database 🥇
+
+      if (currentDocument.uid == user.uid) {
+        console.log(currentDocument.uid);
+        return doc.id;
+      }
+    });
+    return userDocument;
+  };
+
+  const setUserImageURL = async (link) => {
+    const userDocument = await findUser();
+
+    if (userDocument[0] != null || userDocument[0] != undefined) {
+      db.collection("users")
+        .doc(userDocument[0])
+        .update({
+          imageURL: link,
+        })
+        .then(() => {
+          setUserImage(link);
+        });
+    } else {
+      setUserImage("");
+    }
+  };
+
+  const getImageURL = async (userDocument) => {
+    console.log(userDocument);
+    const user = await db.collection("users").doc(userDocument[0]).get();
+    return user.data().imageURL;
+  };
+  useEffect(() => {
+    async function userImageURL() {
+      const userDocument = await findUser();
+      const userImage = await getImageURL(userDocument);
+      setUserImage(userImage);
+    }
+    // only call this method if user is signed in or if the user global state is not empty
+    if (Object.keys(user).length > 0) {
+      userImageURL();
+    } else {
+      setUserImage('');
+    }
+  }, [UserImage, user]);
 
   const addPost = (e) => {
     e.preventDefault();
@@ -19,9 +78,57 @@ function FeedHeader() {
       verified: true,
     });
 
-    setPostImage('')
-    setPostText('')
+    setPostImage("");
+    setPostText("");
   };
+
+  const updateImage = (e) => {
+    // get the file name first
+    const fileName = e.target.files[0].name;
+    const fileType = fileName.split(".");
+
+    // check if user is signed in or not
+    if (user != null) {
+      // user is signed in
+      // so you can upload the image 💅
+      // apply a check on it to see if it is an image or not
+      if (
+        fileType[1] == "jpg" ||
+        fileType[1] == "png" ||
+        fileType[1] == "jpeg"
+      ) {
+        const uploadedImage = storage
+          .ref(`images/${fileName}`)
+          .put(e.target.files[0]);
+        uploadedImage.on(
+          "state_changed",
+          function (snapshot) {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+          },
+          (err) => alert(err.message),
+          async () => {
+            const link = await storage
+              .ref("images")
+              .child(fileName)
+              .getDownloadURL();
+
+            // find the user in the database
+            // set the image url in that user document to the link we get from firebase storage 🩹🅰️👊
+            setUserImageURL(link);
+          }
+        );
+      } else {
+        alert("File type not supported");
+      }
+    } else {
+      alert("Please sign in first to continue");
+    }
+  };
+
   return (
     <div>
       <div className="row card d-flex flex-column border-0 ">
@@ -29,14 +136,50 @@ function FeedHeader() {
           <h5 style={{ fontSize: 22, color: "#00aced" }}>HOME</h5>
         </div>
         <div className="row d-flex flex-row">
-          <div className="col-xl-2 col-lg-2 col-md-4 col-sm-4 mt-2 ">
+          <div className="col-xl-2 col-lg-2 col-md-4 col-4 mt-2 ">
             {/* user avatar 🎱 */}
-            <Avatar
-              className="h-100 w-100 img-fluid"
-              src="https://scontent.fkhi11-1.fna.fbcdn.net/v/t1.0-9/95260674_2556673724590700_5847535705068142592_n.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeFiRRsHNV54VEgiUYuRAKbChzk7UcgGVvyHOTtRyAZW_GRbwC3kA0cf6z_pHq1Qd71FS3MNYPDPT8ur3Yx3T_46&_nc_ohc=GQoUYoCQNQcAX-r6z18&_nc_ht=scontent.fkhi11-1.fna&oh=96f70b15781d5cd5ee2ebb3f8fc076c9&oe=5F8B91F2"
-            />
+            {/* if the user has not set his image yet then show the random avatar  */}
+            {UserImage == "" ? (
+              <Badge
+                badgeContent={"+"}
+                color="primary"
+                className="mt-2"
+                style={{ cursor: "pointer" }}
+                onClick={(e) => imageRef.current.click()}
+              >
+                <Avatar
+                  className="h-100 w-100 img-fluid"
+                  src="https://www.iconfinder.com/data/icons/avatars-with-emotions-boys-avatars-with-different-/283/male-076-512.png"
+                />
+
+                <input
+                  type="file"
+                  ref={imageRef}
+                  onChange={updateImage}
+                  style={{ display: "none" }}
+                />
+              </Badge>
+            ) : (
+              // else set the image according to the link coming from the database
+              <Badge
+                badgeContent={"+"}
+                color="primary"
+                className="mt-2"
+                style={{ cursor: "pointer" }}
+                onClick={(e) => imageRef.current.click()}
+              >
+                <Avatar className="h-100 w-100 img-fluid" src={UserImage} />
+
+                <input
+                  type="file"
+                  ref={imageRef}
+                  onChange={updateImage}
+                  style={{ display: "none" }}
+                />
+              </Badge>
+            )}
           </div>
-          <div className="col-xl-9 col-lg-9 col-md-7 col-sm-8 m-0 p-0 mt-4">
+          <div className="col-xl-9 col-lg-9 col-md-7 col-8 m-0 p-0 mt-4">
             {/* post section 👡 */}
             <input
               type="text"
@@ -50,7 +193,7 @@ function FeedHeader() {
         </div>
         <div className="row mt-2 border-bottom p-2">
           {/* image posting section 🦺 */}
-          <div className="col-10">
+          <div className="col-xs-10 col-lg-9 col-md-8 col-7">
             <input
               type="text"
               className="form-control border-0 border-bottom"
@@ -60,7 +203,7 @@ function FeedHeader() {
               style={{ backgroundColor: "#f8f8f8" }}
             />
           </div>
-          <div className="col-2">
+          <div className="col-xs-2 col-lg-3 col-md-4 col-5">
             <button type="button" className="btn btn-primary" onClick={addPost}>
               Tweet
             </button>
