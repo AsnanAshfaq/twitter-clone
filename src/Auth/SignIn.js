eb9119e94e796f24404dca8d14c16c1442e40d35
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2020-09-24 02:24:53 +0500

    Added the sign in and sign up desing:tada:

---
+++ src/Auth/SignIn.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import { auth } from "../firebase";
+import { useStateValue } from "../StateProvider";
+
+function SignIn() {
+  // global state
+  const [{user}, dispatch] = useStateValue();
+
+  // local states
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const History = useHistory();
+
+  // make the user sign in
+  const userSignIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(Email, Password)
+      .then((user) => {
+        // set the user uid in the context api
+        
+        // console.log(user)
+        History.replace("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <div className="row m-0 p-0">
+            <div className="col d-flex justify-content-center">
+              {/* twitter logo  */}
+              <Link to="/">
+                <TwitterIcon style={{ fontSize: 100, color: "#00aced" }} />
+              </Link>
+            </div>
+          </div>
+          {/* Email  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-start">
+              <h6 className="text-secondary">Email Address</h6>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 w-25 ">
+              <input
+                type="email "
+                className="form-control d-flex justify-content-center"
+                placeholder="Enter Email Address"
+                value={Email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          {/* password  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-start">
+              <h6 className="text-secondary">Password</h6>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+
+          <div className="row">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 w-25 ">
+              <input
+                type="password"
+                maxLength={15}
+                className="form-control d-flex justify-content-center"
+                placeholder="Enter Password"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          {/* sign in button  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-center">
+              <button
+                className="btn btn-primary w-100"
+                disabled={!Email || !Password}
+                onClick={userSignIn}
+              >
+                <h5>Sign In</h5>
+              </button>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-center">
+              <p>
+                Dont't Have an Account?
+                <Link to="/signup"> Sign Up Here</Link>
+              </p>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
---
+++ src/Auth/SignUp.js
@@ -0,0 +1,230 @@
+import React, { useState } from "react";
+import {Link, useHistory} from 'react-router-dom';
+import "./signin.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import {db, auth} from '../firebase';
+// context api 
+import {useStateValue} from '../StateProvider';
+
+function SignUp() {
+
+  // global state 
+  const [{usre}, dispatch] = useStateValue()
+  // local states
+  const [Name, setName] = useState("")
+  const [Username, setUsername] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+
+  const History = useHistory()
+
+  // Create User Account 💯
+  const createUserAccount = (e) => {
+    e.preventDefault()
+
+    // check if name is greater than 5 characters 
+    if(Name.length < 5){
+      alert("Name should not be less than 5 characters")
+    }
+    else if(Username.length < 5){
+      alert("User Name should not be less than 5 characters")
+    }
+    else if(Password.length < 5){
+      alert("Password should not be less than 5 characters")
+    }
+
+    // check if the password and confirm password are same or not 
+    else if(Password != ConfirmPassword){
+      alert("Password's do not match")
+    }
+    else{
+      // create user account 
+      auth
+          .createUserWithEmailAndPassword(Email,Password)
+          .then((user) => {
+            if(user){
+              // add user in the data base with the uid  🍔
+              db.collection("users").doc().set({
+                Name: Name,
+                UserName: Username,
+                imageURL: "",
+                uid : user.user.uid
+              })
+
+              // // set the user in the context api 💯 
+              dispatch({
+                type: 'ADD_USER',
+                user : {
+                  Name: Name,
+                  UserName: Username,
+                  imageURL: "",
+                  uid : user.user.uid
+                }
+              })
+              History.replace("/")
+            }
+          })
+          .catch(err => 
+            alert(err.message)
+            )
+    }
+  }
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <div className="row m-0 p-0">
+            <div className="col d-flex justify-content-center">
+              {/* twitter logo  */}
+              <Link to="/">
+              <TwitterIcon style={{ fontSize: 100, color: "#00aced" }} />
+              </Link>
+            </div>
+          </div>
+          {/* Name  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-start">
+              <h6 className="text-secondary">Name</h6>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 w-25 ">
+              <input
+                type="text"
+                maxLength={15}
+                className="form-control d-flex justify-content-center"
+                placeholder="Enter Your Name"
+                value={Name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          {/* User Name  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-start">
+              <h6 className="text-secondary">User Name</h6>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 w-25 ">
+              <input
+                type="text"
+                maxLength={15}
+                className="form-control d-flex justify-content-center"
+                placeholder="@username"
+                value={Username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          {/* Email  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-start">
+              <h6 className="text-secondary">Email Address</h6>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 w-25 ">
+              <input
+                type="email"
+                className="form-control d-flex justify-content-center"
+                placeholder="Enter Email Address"
+                value={Email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          {/* Password  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-start">
+              <h6 className="text-secondary">Password</h6>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+
+          <div className="row">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 w-25 ">
+              <input
+                type="password"
+                maxLength={15}
+                className="form-control d-flex justify-content-center"
+                placeholder="Enter Password"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          {/* confirm password  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-start">
+              <h6 className="text-secondary">Confirm Password</h6>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+
+          <div className="row">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 w-25 ">
+              <input
+                type="password"
+                maxLength={15}
+                className="form-control d-flex justify-content-center"
+                placeholder="Enter Password"
+                value={ConfirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+          </div>
+          {/* sign up button  */}
+          <div className="row mt-4">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-center">
+              <button
+                className="btn btn-primary w-100"
+                disabled={!Name || !Username || !Email || !Password ||!ConfirmPassword}
+                onClick={createUserAccount}
+              >
+                <h5>Sign Up</h5>
+              </button>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            
+          </div>
+          <div className="row mt-2">
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-8 d-flex justify-content-center">
+              <p>Already Have an Account.
+                  <Link to="/signin">
+                   Sign In Here
+                  </Link>
+              </p>
+            </div>
+            <div className="col-xs-4 col-lg-4 col-md-4 col-2"></div>
+            
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
